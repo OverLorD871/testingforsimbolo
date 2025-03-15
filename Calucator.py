45b94830488ff55b7224ede14e666caffe26198a
OverLorD871 <91267845+OverLorD871@users.noreply.github.com>
2025-03-16 01:27:20 +0630

    Add files via upload
    This is a calculator which I developed with streamlit

---
+++ Calucator.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import numpy as np
+
+number = st.number_input('Insert a Number')
+st.write('The Current Number is', number)
+
+snumber = st.number_input('Insert a second Number')
+
+addition = (number + snumber)
+multiply = (number * snumber)
+subtraction = (number - snumber)
+division = (number / snumber) if snumber != 0 else 'undefined'
+
+left, middle, middle2, right = st.columns(4)
+
+if left.button("Add", use_container_width=True):
+    left.markdown(f'The addition of {number} and {snumber} is {addition}')
+
+if middle.button("Multiply", use_container_width=True):
+    middle.markdown(f'The multiplication of {number} and {snumber} is {multiply}')
+
+if middle2.button("Subtract", use_container_width=True):
+    middle2.markdown(f'The subtraction of {number} and {snumber} is {subtraction}')
+
+if right.button("Divide", use_container_width=True):
+    right.markdown(f'The division of {number} and {snumber} is {division}')
+
